d1351cb9552eb91c470bb383cc8bbe79df3d4e7d
Yonti96 <xjonathanklein@gmail.com>
2023-02-18 22:41:53 +0200

    Docs and Sketches

---
+++ src/models/pageModel.js
@@ -0,0 +1,10 @@
+const PAGES = {
+    HOME: "home-page",
+    ABOUT: "about-page",
+    CREATE_PIC: "create-pic-page",
+    LOGIN: "login-page",
+    SIGNUP: "signup-page",
+    ERROR_404: "error-page"
+};
+
+export default PAGES;
---
+++ src/services/domService.js
@@ -0,0 +1,15 @@
+// **** PAGES ****/
+export const HOME_PAGE = document.getElementById("home-page");
+export const ABOUT_PAGE = document.getElementById("about-page");
+export const CREATE_PIC = document.getElementById("create-pic-page");
+export const LOGIN = document.getElementById("login-page");
+export const SIGNUP = document.getElementById("signup-page");
+export const ERROR_404 = document.getElementById("error-page");
+
+// **** PAGES - LINKS ****/
+export const HOME_PAGE_LINK = document.getElementById("home-page-link");
+export const ABOUT_PAGE_LINK = document.getElementById("about-page-link");
+export const CREATE_PIC_LINK = document.getElementById("create-pic-link");
+export const LOGIN_PAGE_LINK = document.getElementById("login-page-link");
+export const SIGNUP_PAGE_LINK = document.getElementById("sign-up-link");
+export const ERROR404_PAGE_LINK = document.getElementById("error404-page-link");
