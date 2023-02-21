5d2845c0311f02414971c0a1aa6fb21c72f4a3fb
Yonti96 <xjonathanklein@gmail.com>
2023-02-21 10:37:09 +0200

    Fixed Typos

--- src/app.js
+++ src/app.js
@@ -6,19 +6,19 @@ import onChangePageSliderPic from "./pictures/helpers/onChangeSlderPic.js";
 
 const pictures = [
     {
-    url: "https://cdn.pixabay.com/photo/2023/01/31/05/59/zebra-7757193_960_720.jpg",
-    alt: "zebra",
-    credits: "Jessica rabbit",
+url: "https://cdn.pixabay.com/photo/2023/01/31/05/59/zebra-7757193_960_720.jpg",
+alt: "zebra",
+credits: "Jessica rabbit",
     },
     {
-    url: "https://cdn.pixabay.com/photo/2023/01/23/09/26/cat-7738210_960_720.jpg",
-    alt: "cat",
-    credits: "hillary clinton",
+url: "https://cdn.pixabay.com/photo/2023/01/23/09/26/cat-7738210_960_720.jpg",
+alt: "cat",
+credits: "hillary clinton",
     },
     {
-    url: "https://cdn.pixabay.com/photo/2023/01/27/06/17/pheasant-7747830_960_720.jpg",
-    alt: "pigeon",
-    credits: "shula zaken",
+url: "https://cdn.pixabay.com/photo/2023/01/27/06/17/pheasant-7747830_960_720.jpg",
+alt: "pigeon",
+credits: "shula zaken",
     },
 ];
 
@@ -26,11 +26,11 @@ let counter = setCounter(pictures, 0); //0
 renderSlider(pictures, counter);
 
 SLIDER_NEXT_BTN.addEventListener(
-    "click",
-    () => (counter = onChangePageSliderPic(pictures, counter, "next"))
+"click",
+() => (counter = onChangePageSliderPic(pictures, counter, "next"))
 );
 
 SLIDER_PREV_BTN.addEventListener(
-    "click",
-    () => (counter = onChangePageSliderPic(pictures, counter, "prev"))
+"click",
+() => (counter = onChangePageSliderPic(pictures, counter, "prev"))
 );
