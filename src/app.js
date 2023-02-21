5dfc87770af8868176a9c620575176d35eeeac93
Yonti96 <xjonathanklein@gmail.com>
2023-02-21 10:30:07 +0200

    Next+Prev Btns

--- src/app.js
+++ src/app.js
@@ -1,1 +1,36 @@
 import "./routes/links.js";
+import setCounter from "./pictures/helpers/setCounter.js";
+import renderSlider from "./pictures/components/renderSlider.js";
+import { SLIDER_NEXT_BTN, SLIDER_PREV_BTN } from "./services/domService.js";
+import onChangePageSliderPic from "./pictures/helpers/onChangeSlderPic.js";
+
+const pictures = [
+    {
+    url: "https://cdn.pixabay.com/photo/2023/01/31/05/59/zebra-7757193_960_720.jpg",
+    alt: "zebra",
+    credits: "Jessica rabbit",
+    },
+    {
+    url: "https://cdn.pixabay.com/photo/2023/01/23/09/26/cat-7738210_960_720.jpg",
+    alt: "cat",
+    credits: "hillary clinton",
+    },
+    {
+    url: "https://cdn.pixabay.com/photo/2023/01/27/06/17/pheasant-7747830_960_720.jpg",
+    alt: "pigeon",
+    credits: "shula zaken",
+    },
+];
+
+let counter = setCounter(pictures, 0); //0
+renderSlider(pictures, counter);
+
+SLIDER_NEXT_BTN.addEventListener(
+    "click",
+    () => (counter = onChangePageSliderPic(pictures, counter, "next"))
+);
+
+SLIDER_PREV_BTN.addEventListener(
+    "click",
+    () => (counter = onChangePageSliderPic(pictures, counter, "prev"))
+);
